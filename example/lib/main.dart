06cffd80646f9465b79f470a3323afc429b851bd
Jamiu Okanlawon <developerjamiu@gmail.com>
2024-03-12 17:47:52 +0100

    chore: add a more elaborate example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,21 +5,84 @@ void main() {
   runApp(const SmartTextFlutterExample());
 }
 
-class SmartTextFlutterExample extends StatelessWidget {
+class SmartTextFlutterExample extends StatefulWidget {
   const SmartTextFlutterExample({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    const text =
-        'Here is a text with an address: 36 Lagos Street written at 9PM by someone with phone: +2340000000000 and you can reach him at reaching@email.com or you can check twitter.com';
+  State<SmartTextFlutterExample> createState() =>
+      _SmartTextFlutterExampleState();
+}
 
+class _SmartTextFlutterExampleState extends State<SmartTextFlutterExample> {
+  final List<String> messageTexts = [];
+
+  late final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Smart Text Flutter'),
         ),
-        body: const Center(
-          child: SmartText(text),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: messageTexts.length,
+                itemBuilder: (context, index) => Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.sizeOf(context).width * 0.8,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.blueGrey.shade50,
+                    ),
+                    padding: const EdgeInsets.all(8),
+                    margin: const EdgeInsets.all(8),
+                    child: SmartText(
+                      messageTexts[index],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _messageController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter text',
+                        contentPadding: EdgeInsets.all(8),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() => messageTexts.add(_messageController.text));
+                      _messageController.clear();
+                    },
+                    child: const Text('Send Message'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+          ],
         ),
       ),
     );
