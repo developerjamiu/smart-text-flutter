bdfe4f754a19d79a8a96fd4b21bf6de3d1db0038
Jamiu Okanlawon <developerjamiu@gmail.com>
2024-08-05 20:08:10 +0100

    chore: export smart selectable text

--- lib/smart_text_flutter.dart
+++ lib/smart_text_flutter.dart
@@ -2,3 +2,4 @@ export 'src/smart_text_flutter.dart';
 export 'src/models/item_span.dart';
 export 'src/models/item_span_config.dart';
 export 'src/widgets/smart_text.dart';
+export 'src/widgets/smart_selectable_text.dart';
