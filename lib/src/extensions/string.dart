3099ecfb9d94e86b02308db3a1332a497e466653
Jamiu Okanlawon <developerjamiu@gmail.com>
2025-01-23 11:29:37 +0100

    fix: return urls without protocol as is

--- lib/src/extensions/string.dart
+++ lib/src/extensions/string.dart
@@ -1,6 +1,7 @@
 extension StringX on String {
   String humanizeUrl(bool humanize) {
     if (!humanize) return this;
+    if (!startsWith('http')) return this;
 
     final uri = Uri.parse(this);
 
